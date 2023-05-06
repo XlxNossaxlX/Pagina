1607ce3c267df9059127fefc2f3babef4e8abf9c
XlxNossaxlX <130846623+XlxNossaxlX@users.noreply.github.com>
2023-05-06 14:47:11 -0500

    Update script.js

--- Proyecto/login/script.js
+++ Proyecto/login/script.js
@@ -12,23 +12,4 @@ function activarLink(){
 list.forEach((item)=>
 item.addEventListener('click',activarLink)
 )
-function myFunction(){
-    var input,filte,table,tr,td,i,txtValue;
-    input=document.getElementById("myInput");
-    filte=input.Value.toUpperCase();
-    table=document.getElementById("myTable");
-    tr=table.getElementsByTagName("tr");
 
-    for(i = 0; i < tr.length; i++){
-        td= tr[i].getElementsByTagName("td")[0];
-        if (td){
-            txtValue = td.textContent || td.innerText;
-            if(txtValue.toUpperCase().indexOf(filter) > -1)
-            {
-                tr[i].style.display="";
-            }else{
-                trt[i].style.display="none";
-            }
-        }        
-    }
-}
